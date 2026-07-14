ba40dca97bd169a0231ca618aa50c03b83de3a56
Basil T <basilribumon@gmail.com>
2026-07-14 15:40:16 +0530

    refactor: Remove unnecessary blank lines in AdminOrders and AdminProducts for improved readability

--- src/admin/pagess/adminOrders.jsx
+++ src/admin/pagess/adminOrders.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 import AdminLayout from "../../layouts/AdminLayout";
-
 import { fetchAllOrders, updateOrderStatus } from "../redux/adminOrderSlice";
 
 function AdminOrders() {
--- src/admin/pagess/adminProducts.jsx
+++ src/admin/pagess/adminProducts.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 import AdminLayout from "../../layouts/AdminLayout";
 import {
   fetchProducts,
